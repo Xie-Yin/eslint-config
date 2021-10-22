b8d65e9c6b975ce14fc9c555383b127448aa4dcb
Mario34 <zhl.mario34@outlook.com>
2021-10-23 00:55:25 +0800

    feat: update ts

--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -1,6 +1,5 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  parserOptions: { sourceType: 'module', ecmaVersion: 2020, project: ['tsconfig.json'] },
   plugins: ['@typescript-eslint'],
   extends: [
     '@xyfebaby/eslint-config-base/rules/style',
